d1c0a6db50039d9a10d0489f45679b3ee1a77ff0
GH-546417 <546417@gmail.com>
2025-05-29 16:44:16 -0500

    Cambios en el script stage0_loading.py

--- src/data_eng/stage0_loading.py
+++ src/data_eng/stage0_loading.py
@@ -1,6 +1,9 @@
 import sys
+import os
 import pandas as pd
 import argparse
+import requests
+
 from app_logging import logging
 from app_exception.exception import AppException
 
@@ -8,35 +11,52 @@ class GetData:
     
     '''
     The main functionality is to get data from external source 
-    Function return None 
+    Function return data 
     '''
 
     def __init__(self):
         pass
 
-
     def get_data(self, config_path):
         try:
-            logging.info("getting the data from the external source")
-            #self.config = self.read_params(config_path)
-            
+            logging.info("Getting the data from the external source")
 
-            self.data_path = config_path
+            # URL del archivo CSV en GitHub
+            # aquí se asume que config_path es la URL del archivo CSV
+            github_csv_url = config_path
+            
+            # Ruta local donde se guardará el archivo
+            self.external_data = "data/external"
+            os.makedirs(self.external_data, exist_ok=True)
+            local_path = os.path.join(self.external_data, "Consignment_pricing.csv")
+
+            # Descargar el archivo CSV desde GitHub
+            response = requests.get(github_csv_url)
+            response.raise_for_status()  # lanza una excepción si la descarga falla
+
+            with open(local_path, 'wb') as f:
+                f.write(response.content)
+
+            logging.info("CSV file downloaded successfully")
+
+            # Leer el archivo CSV descargado
+            self.data_path = local_path
+            print (local_path)
             self.data = pd.read_csv(self.data_path, sep=',', encoding='utf-8')
-        
-            logging.info(f"Data Fetched from the source Successfully !!!")
+
+            logging.info("Data successfully uploaded from downloaded file")
             return self.data
 
-
         except Exception as e:
-            print(
-                f"Exception Occurred while getting data from the source -->{e}")
+            logging.error(f"Exception occurred while getting data from the source --> {e}")
             raise AppException(e, sys) from e
+
 
 
 if __name__ == "__main__":
 
     args = argparse.ArgumentParser()
-    args.add_argument('--input_path', default='data/external/Consignment_pricing.csv')
+    args.add_argument('--input_path', default="https://raw.githubusercontent.com/jmem-ec/KRRCourse/ccbd6ccf8389ba0988d53fc9300a64da00e6368b/Consignment_pricing.csv")
+    
     parsed_args = args.parse_args()
     data = GetData().get_data(config_path=parsed_args.input_path)
